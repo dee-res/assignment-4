81a5202b0634f5b94610cabb729273dcc2edc119
dee-res <restrepo.diana.pat@gmail.com>
2024-09-26 11:42:20 -0700

    Update SpeakHello.js

--- easier/SpeakHello.js
+++ easier/SpeakHello.js
@@ -4,6 +4,8 @@
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
     
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
