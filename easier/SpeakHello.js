86dbcba454044de7f80dddf46c1c5b7b803384f1
dee-res <restrepo.diana.pat@gmail.com>
2024-09-26 12:05:57 -0700

    Update SpeakHello.js

--- easier/SpeakHello.js
+++ easier/SpeakHello.js
@@ -1,11 +1,13 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function () {
+(function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 var speakWord = "Hello";   
-function speak(name) {
+speakWord.sayHello function speak(name) {
   console.log(speakWord + " " + name);
 }
+  window.speakWord = speakWord;
+}) (window);
   
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
