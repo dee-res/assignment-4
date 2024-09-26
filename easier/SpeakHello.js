3838bcc5ae7b69e27f786e50c37c66df3a2f7bb3
dee-res <restrepo.diana.pat@gmail.com>
2024-09-26 11:33:59 -0700

    Update SpeakHello.js

--- easier/SpeakHello.js
+++ easier/SpeakHello.js
@@ -1,6 +1,15 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
+(function (window) {
+  var helloSomeName = {};
+  helloSomeName.name = "";
+  var greeting = "Hello";
+  helloSomeName.sayHello = function () {
+    console.log(greeting + helloSomeName.name);
+  }
+  window.helloSomeName = helloSomeName;
+})(window);
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
